b67cbe95c6d9e7e14061e01a4294b4b0182789a6
CapKarel <cap.karel@zak.spspb.cz>
2026-03-17 12:14:18 +0100

    Update init.php

--- init.php
+++ init.php
@@ -1,1 +1,17 @@
+<?php
+session_start();
 
+try {
+    $db = new PDO("sqlite:profile.db");
+    // Nastavení vyhazování výjimek v případě chyby
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Vytvoření tabulky, pokud neexistuje
+    $query = "CREATE TABLE IF NOT EXISTS interests (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE
+              )";
+    $db->exec($query);
+} catch (PDOException $e) {
+    die("Chyba databáze: " . $e->getMessage());
+}
